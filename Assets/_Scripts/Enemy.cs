e690f749f56c17a65a29b95f0d225a8e35d4d69c
cek101 <cek101@scarletmail.rutgers.edu>
2018-11-02 01:36:48 -0400

    1st submission
    enemy objects do not collide with the player.

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Set in Inspector: Enemy")]
+    public float speed = 10f;  // the speed in m/s
+    public float fireRate = 0.3f; // Seconds/shot (Unused)
+    public float health = 10;
+    public float score = 100; // Points earned for destroy this
+
+    private BoundsCheck bndCheck;
+
+    
+    void Awake()    {
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    // this is a Property: A method that acts like a field
+    public Vector3 pos  {
+        get {
+            return (this.transform.position);
+        }
+        set {
+            this.transform.position = value;
+        }
+    }
+    // Update is called once per frame
+    void Update()   {
+        Move();
+
+        if (bndCheck != null && !bndCheck.offDown)  {
+            //if ( bndCheck != null && !bndCheck.isOnScreen )  {
+            // check to make sure it's gone off the bottom of the screen
+            if ( pos.y <-bndCheck.camHeight - bndCheck.radius )  {
+                // We're off the bottom, so destroy this GameObject
+                Destroy(gameObject);
+            }
+        }
+    } 
+
+    public virtual void Move() {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+    void OnCollisionEnter( Collision coll) {
+            GameObject otherGO = coll.gameObject;
+        Debug.Log("Collided!!!");
+            if ( otherGO.tag == "ProjectileHero" ) {
+                Destroy( otherGO ); // destroy the projectile 
+                Destroy(gameObject);  //destroy the enemy gameobject
+            } else {
+                print("Enemy hit by non-ProjectileHero: " + otherGO.name);
+            }
+    }
+}
+
---
+++ Assets/_Scripts/Main.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections; // required for arrays and other collections
+using System.Collections.Generic; // required to use Lists or dictionaries
+using UnityEngine; // required for unity
+using UnityEngine.SceneManagement;  // for loading and reloading of scenes
+
+public class Main : MonoBehaviour {
+    static public Main S; // singleton for main
+
+    [Header("Set in Inspector")]
+    public GameObject[] prefabEnemies; // array of enemy prefabs
+    public float enemySpawnPerSecond = 0.5f; // # enemies/second
+    public float enemyDefaultPadding = 1.5f; // padding for position
+
+    private BoundsCheck bndCheck;
+
+	// Use this for initialization
+	void Awake () {
+        S = this;
+        // set bndCheckto reference the BoundsCheck component on this GameObject
+        bndCheck = GetComponent<BoundsCheck>();
+        // invoke SpawnEnemy () once (in 2 seconds, based on default values)
+        Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+	}
+
+    public void SpawnEnemy() {
+        //Debug.Log("Spawningsomethiong at" + Time.time);
+        // pick a random Enemy prefab to instantiate
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]); //Error here
+
+        // position the enemy above the screen with a random x position
+        float enemyPadding = enemyDefaultPadding; // book typo?
+        //float enemyDefaultPadding; 
+        if (go.GetComponent<BoundsCheck>() != null) {
+            enemyPadding = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
+
+            // set the initial position for the spawned enemy
+            Vector3 pos = Vector3.zero;
+            float xMin = -bndCheck.camWidth + enemyPadding;
+            float xMax = bndCheck.camWidth - enemyPadding;
+            pos.x = Random.Range(xMin, xMax);
+            pos.y = bndCheck.camHeight + enemyPadding;
+            go.transform.position = pos;
+
+            // invoke SpawnEnemy () again
+            Invoke("SpawnEnemy", 1f / enemySpawnPerSecond);
+        }
+    }
+
+    public void DelayedRestart(float delay) {
+        // invoke the restart () method in delay seconds
+        Invoke("Restart", delay);
+    }
+
+    public void Restart() {
+        // reload _Scene_0 to restart the game
+        SceneManager.LoadScene("_Scene_0");
+    }
+} // first bracket
+
+		
+
